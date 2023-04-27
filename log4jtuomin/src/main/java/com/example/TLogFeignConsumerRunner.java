c0a57b174be62a1ff6a7c29130277850cb45ad00
夕久 <3090018282@qq.com>
2023-04-27 17:48:24 +0800

    日志脱敏

---
+++ log4jtuomin/src/main/java/com/example/TLogFeignConsumerRunner.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TLogFeignConsumerRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TLogFeignConsumerRunner.class, args);
+
+    }
+}
